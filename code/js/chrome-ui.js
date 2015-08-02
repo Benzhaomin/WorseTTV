77f6cfa9a5eddc0985bb1f7a14fc5ab8b595f4e1
Ben <ben@wainei.net>
2015-08-02 15:15:41 +0200

    [bugfix] only show the context menu on twitch

--- code/js/chrome-ui.js
+++ code/js/chrome-ui.js
@@ -17,17 +17,24 @@
     states[tab] = !states[tab];
   };
 
+  /*chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
+    if (tab.url.indexOf("http://translate.google.hu/") > -1 &&
+        changeInfo.url === undefined){
+      chrome.tabs.executeScript(tabId, {file: "program.js"} );
+    }
+  });*/
+
   var contextMenu;
 
   var onContextMenuClicked = function(info, tab) {
 
     if (!is_started(tab.id)) {
-      //console.log("[chrome-ui] started curing");
+      //console.log("[chrome-ui] start curing plz");
 
       chrome.tabs.sendMessage(tab.id, {"message": "worsettv.chat.observer.start"});
     }
     else {
-      //console.log("[chrome-ui] stopped curing");
+      //console.log("[chrome-ui] stop curing plz");
 
       chrome.tabs.sendMessage(tab.id, {"message": "worsettv.chat.observer.stop"});
     }
@@ -39,6 +46,7 @@
   contextMenu = chrome.contextMenus.create({
     "title": "Toggle Cancer",
     "contexts":["page"],
+    "documentUrlPatterns": ["http://www.twitch.tv/*"],
     "onclick": onContextMenuClicked
   });
 
