79772064ad0b085894043436ee9177a8a0f3bac9
Ben <ben@wainei.net>
2016-07-04 19:53:34 +0200

    [build] updated dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -154,7 +154,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-exec');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-jpm-modern');
 
